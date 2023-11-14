fa69f2fa7049427b912470ed814437166a5ba40a
gustavo.pereira <gustavo.pereira@zencloud.com.br>
2023-11-14 17:43:15 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'fut-mais';
+  
 }
+const firebaseConfig = {
+  apiKey: "AIzaSyAFV8jRdARdaGxsU-ET_5gQkm2Gzf75bGE",
+  authDomain: "futmais-7da3f.firebaseapp.com",
+  projectId: "futmais-7da3f",
+  storageBucket: "futmais-7da3f.appspot.com",
+  messagingSenderId: "476442590443",
+  appId: "1:476442590443:web:246aa3c2bf0210723f52ae",
+  measurementId: "G-S61P40S58C"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
